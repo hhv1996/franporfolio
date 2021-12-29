7b930f20c32658d02c8e81fbe02867b3d30da55f
Hugo Hernan Vargas <HVargas@DTVPAN.COM>
2021-12-29 17:16:26 -0300

    primer comit home a terminar

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,32 @@
+import React, { useState } from 'react'
+  
+import './Home.css'
+
+const Home = () => {
+    const [sDescription,setSDescription] = useState ("Franco gigena mejor conocido como colorme makeup en sus redes, es un cosplayer especializado en el arte de la caracterizacion con maquillaje y protesis, además de ser peluquero, manicura, diseñador de vestuario y profesor de dichas áreas y próximamente profesor de artes plásticas.")
+    const [lDescription,setLDescription] = useState ("Franco gigena mejor conocido como colorme makeup en sus redes, es un cosplayer especializado en el arte de la caracterizacion con maquillaje y protesis, además de ser peluquero, manicura, diseñador de vestuario y profesor de dichas áreas y próximamente profesor de artes plásticas. La mayoría de sus personajes, con los cuales ganó concursos  son de índole femenina (crossplay) encontrando personajes como morticia addams, rita repulsa, hiedra venenosa, avatar kyoshi , ursula y muchas más!!! A los cuales siempre logra llevar al extremo no solo en detalles de makeup sino de modificación corporal para lograr la máxima excelencia y detalles de trajes.")
+    const [textControl, setTextControl] = useState (true)
+    const controlText=()=>{
+        if (textControl) {
+            setTextControl(false)
+        }else{
+            setTextControl(true)
+        }
+    }
+    return (
+        <div className='homeContainer'>
+            <div className='aboutMe'>
+                <h3 className='greeting'>Hola, soy Franco Gigena</h3>
+                <h1 className='profession'>Cosplayer. Estilista. </h1>
+                <p className='descriptionAboutMe'>{textControl?sDescription:lDescription}</p>
+                <button className='buttonReadMore' onClick={controlText}>{textControl?"Leer mas...":"Esconder..."}</button>
+
+            </div>
+            <div className='imgPerfilContainer'>
+                <img className='imgPerfil' src="./img/perfil2.png" alt="" />
+            </div>
+        </div>
+    )
+}
+
+export default Home
