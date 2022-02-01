063811a21d1078f73a105f9651355480b5cc9b90
Hugo Hernan Vargas <HVargas@DTVPAN.COM>
2022-02-01 17:02:25 -0300

    se termino el jonslist

--- src/components/Jobs/JobList/JobList.jsx
+++ src/components/Jobs/JobList/JobList.jsx
@@ -1,5 +1,8 @@
 import React, { useEffect, useState } from 'react'
+import { Plock } from "react-plock";
+import { MdVisibility } from "react-icons/md";
 import './JobList.css'
+
 
 const JobList = ({jobs}) => {
     const [listJob, setListJob] = useState([])
@@ -18,19 +21,29 @@ const JobList = ({jobs}) => {
         });
         setListJob(auxJobs)
       }, [jobs])
-    
+      const breakpoints = [
+        { size: 640, columns: 1 },
+        { size: 768, columns: 2 },
+        { size: 1024, columns: 4 }
+      ];
     return (
-        <div className='jobListContainer'>
+        <Plock nColumns={breakpoints} gap={10} className='jobListContainer'>
             {
                 jobs.map(j=>{
                     return(
                         <div  className='jobListItem'>
                             <img className="jobImg" src={j.img} alt={j.title}/>
+                            <div>     
+                                <div className='jobListButtoms'>
+                                    <p className='jobListDescription'>{j.title}</p>
+                                    <MdVisibility className='jobListVisibilityIcon'/>
+                                </div>
+                            </div>
                         </div>
                     )
                 })
             }
-        </div>
+        </Plock>
     )
 }
 
